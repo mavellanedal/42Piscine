fc6c5713ed50b5d641199b0f6d859bea7690c6cf
Miquel Avellaneda Luque <mavellan@car8s1.42barcelona.com>
2024-10-17 13:03:53 +0200

    C06

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mavellan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 15:07:35 by mavellan          #+#    #+#             */
+/*   Updated: 2024/07/19 11:57:23 by mavellan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int	i;
+	int	swapped;
+
+	swapped = 1;
+	while (swapped == 1)
+	{
+		swapped = 0;
+		i = 1;
+		while (i < argc -1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				ft_swap(&argv[i], &argv[i + 1]);
+				swapped = 1;
+			}
+			i++;
+		}
+	}
+}
+
+void	ft_print_params(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	ft_sort_params(argc, argv);
+	while (i < argc)
+	{
+		ft_print_params(argv[i]);
+		i++;
+	}
+	return (0);
+}
